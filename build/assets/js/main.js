698e3b3748c166a1974f2bce4303068d079009cc
Felix <felixfreidman@gmail.com>
2022-04-17 12:59:31 +0800

    12:59PM Commit Well, That's all for nopw

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -95,3 +95,84 @@ var swiper = new Swiper("#greetingSwiper", {
     prevEl: ".swiper-button-prev"
   }
 });
+var menuTogglers = document.querySelectorAll(".content-toggler");
+var screens = document.querySelectorAll(".adaptive-screen"); // const serviceButton = document.getElementById("serviceButton");
+// const shopButton = document.getElementById("shopButton");
+
+var menuButtons = document.querySelectorAll(".button-toggler");
+menuTogglers.forEach(function (toggler) {
+  toggler.addEventListener("click", function () {
+    menuTogglers.forEach(function (button) {
+      if (button.classList.contains("content-toggler--active")) {
+        button.classList.remove("content-toggler--active");
+      }
+    });
+    toggler.classList.add("content-toggler--active");
+    showScreen(toggler.id);
+  });
+});
+
+function showScreen(togglerID) {
+  screens.forEach(function (screen) {
+    if (screen.classList.contains("screen-active")) {
+      screen.classList.remove("screen-active");
+      screen.classList.add('screen-hidden');
+    }
+
+    var screenID = screen.id;
+
+    if (screenID.includes(togglerID)) {
+      screen.classList.add("screen-active");
+      screen.classList.remove('screen-hidden');
+    }
+  });
+}
+
+menuButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    console.log('clicked');
+    menuButtons.forEach(function (specialButton) {
+      if (specialButton.classList.contains("button-toggler--active")) {
+        console.log("changed");
+        specialButton.classList.remove("button-toggler--active");
+      }
+    });
+
+    if (!button.classList.contains("button-toggler--active")) {
+      button.classList.add("button-toggler--active");
+    }
+
+    if (button.id.includes("service")) {
+      showScreen("kardan");
+      menuTogglers.forEach(function (button) {
+        if (button.classList.contains("content-toggler--active")) {
+          button.classList.remove("content-toggler--active");
+        }
+      });
+      document.getElementById("kardan").classList.add('content-toggler--active');
+    } else {
+      showScreen("shop");
+    }
+  });
+});
+
+if (window.location.href.includes("contact")) {
+  var init = function init() {
+    var myMap = new ymaps.Map('map', {
+      center: [56.81122355, 60.72763708],
+      zoom: 14
+    }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      balloonContent: 'г. Иркутск, ул. Сурнова, 20',
+      iconCaption: 'г. Иркутск, ул. Сурнова, 20'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: "./assets/images/content/logo_custom.svg",
+      iconImageSize: [30, 42],
+      iconImageOffset: [-5, -38]
+    });
+    myMap.geoObjects.add(myPlacemark);
+  };
+
+  ymaps.ready(init);
+}
